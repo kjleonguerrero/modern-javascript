160c6aada70e2d4ee78d39527f47680d3a2a8eec
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-11-03 03:26:09 +1000

    added main.js and mathjs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var moment = require("moment");
+var math = require("mathjs");
+
 console.log("Hello from JS!");
+var datetime = moment().startOf("day").fromNow();
+console.log(datetime);
+var dateTimed = moment().format("MMMM Do YYYY, h:mm:ss a");
+console.log(dateTimed);
+
+console.log(math.sqrt(-4));
